cb5421f52378687d305e841c770443224b27fea5
EasyGoingLove <50263561+EasyGoingLove@users.noreply.github.com>
2021-06-28 06:50:59 +0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import session from 'express-session';
 import db from './db/db.js';
 import bodyParser from 'body-parser';
 import router from './routes/router.js';
 import authRouter from './routes/auth.js';
 import cors from 'cors';
+import flash from 'req-flash';
 
 
 db.connect((err) => {
@@ -15,7 +17,8 @@ const app = express();
 
 
 app.use(cors());
-
+app.use(session({secret: '{secret}', name: 'session_id', saveUninitialized: true, resave: true}));
+app.use(flash());
 app.use(express.json());
 app.use(bodyParser.urlencoded())
 
--- backend/controllers/authRegister.js
+++ backend/controllers/authRegister.js
@@ -2,27 +2,28 @@ import db from '../db/db.js';
 import bcrypt from 'bcryptjs';
 
 
+
 const register = (req,res) => {
   console.log(req.body);
-  const {name,pass,rep_pass} =req.body;
+  const {name,pass,rep_pass} =req.body.user;
 
   db.query('SELECT name FROM users WHERE name = ?', [name], async (error,results)=>{
     if(error){console.log(error)}
-   
+      
      if(Object.keys(results).length !== 0 && results.constructor !== Object){
-        return res.json("User already exists with this name.");
+        return res.status(200).send({message: 'This name is already taken'})   
     }
      else if (pass !== rep_pass){
-        return res.json("Passwords don't match");
+        return res.status(200).send({message: "Passwords don't match"}) 
     }
-    else if (name === undefined || name === null || name ==="" ||name ===NaN){
-        return res.json("Name field is empty");
+    else if (!name){
+        return res.status(200).send({message: "Name field is empty"}) 
     }
-    else if (pass === undefined || pass === null || pass ==="" ||pass ===NaN){
-        return res.status(200).send({message: 'mssg'})
+    else if (!pass){
+        return res.status(200).send({message: 'Password field is empty'})
     }
-    else if (rep_pass === undefined || rep_pass === null || rep_pass ==="" ||rep_pass ===NaN){
-        return res.json("Repeat password field is empty");
+    else if (!rep_pass){
+        return res.status(200).send({message: 'Repeat Password field is empty'})
     }
 
     let hashedPass = await bcrypt.hash(pass, 8);
