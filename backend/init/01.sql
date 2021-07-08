741c842f1cf108c855083cdb87d5e438e5e86ec4
Tsvetomir <tsvetomirstoev8@gmail.com>
2021-07-08 06:32:09 +0300

    Updated init for mysql tables

--- backend/init/01.sql
+++ backend/init/01.sql
@@ -1,5 +1,11 @@
-CREATE TABLE patladjan (
+CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(255),
     password VARCHAR(255)
+);
+
+CREATE TABLE arcadepoints (
+    id INT(255),
+    wins VARCHAR(255),
+    losses VARCHAR(255)
 );
